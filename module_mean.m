ecfc422d48adae0487b9b1a8cd291462adcfd0fe
Ruud (Rudy) van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2019-07-30 11:49:19 +0200

    Update module_mean.m

--- module_mean.m
+++ module_mean.m
@@ -1,11 +1,45 @@
-function M = module_mean(M,modz,collapse)
-%usage: M = module_mean(M,modz,collapse)
+function M = module_mean(M,modz,varargin)
+%usage: M = module_mean(M,modz,collapse,incldiag)
 
-M(logical(size(M))) = NaN; %set diagonal to NaN
 
+%% Check input
+
+if isempty(varargin) 
+    collapse = 0;
+    incldiag = 0;
+else    
+    if nargin < 2
+        error('not enough input arguments')    
+    elseif nargin == 3
+        collapse = varargin{1};
+        incldiag = 0;
+    elseif nargin == 4
+        collapse = varargin{1};
+        incldiag = varargin{2};
+    elseif nargin > 4
+        error('too many input arguments')
+    end
+end
+
+if size(M,1) ~= size(M,2)
+    error('input matrix M must be square')
+end
+
+if length(modz) ~= size(M,1) || ~isvector(modz)
+    error('input modz must be a vector and same length as input matrix M')
+end
+
+%% Compute module mean
+
+if ~incldiag
+    M(logical(eye(size(M)))) = NaN; %set diagonal to NaN so that we exclude it from the mean
+end
+
+%find borders between the modules
 intersections = find(diff(modz));
 intersections = [0 intersections length(modz)];
 
+%compute the average value per module-module elements
 if collapse
     Mz = zeros(length(intersections)-1);    
     for mi = 1:length(intersections)-1
